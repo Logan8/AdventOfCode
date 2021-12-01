abc707ab5e3682d5dad64cfe6971a30174baa581
Neil Logan <logan8@gmail.com>
2021-12-01 21:14:45 +0000

    Day one puzzle one - solved

---
+++ AdventOfCode/DayOne.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode
+{
+    class DayOne
+    {
+        const string input = "Resources/DayOneInput.txt";
+
+        public static void PuzzleOne()
+        {
+            var content = File.ReadAllLines(input);
+
+            var first = 0;
+            var second = 0;
+            var increases = 0;
+
+            foreach (var line in content)
+            {
+                if (first == 0 && second == 0)
+                {
+                    Int32.TryParse(line, out first);
+                }
+                else
+                {
+                    Int32.TryParse(line, out second);
+                    if (second > first) increases++;
+                    first = second;
+                }
+            }
+
+            Console.WriteLine("Increased " + increases.ToString() + " times.");
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var running = true;
+            while (running)
+            {
+                Console.WriteLine("Enter which puzzle you want to run in a DAY-PUZZLE format.");
+                Console.WriteLine("(E.g. for day 1, puzzle 1 write 1-1)");
+                var puzzleNumer = Console.ReadLine();
+                switch (puzzleNumer.ToLower())
+                {
+                    case "1-1":
+                        DayOne.PuzzleOne();
+                        break;
+
+                    case "exit":
+                        running = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("I haven't done that puzzle yet... Try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
